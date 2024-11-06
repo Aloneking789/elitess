bad62da95b3a8b5010f3f36cf51c70532b8f2a34
Abhijeet <iamgenious21o9@gmail.com>
2024-11-06 23:42:23 +0530

    added contact us logic

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,67 +1,17 @@
-import { useState, useEffect } from "react";
 import { FaLocationArrow } from "react-icons/fa6";
-import emailjs from "@emailjs/browser";
+
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 
 const Footer = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [formData, setFormData] = useState({
-    name: "",
-    message: "",
-    email: "",
-  });
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setFormData({
-      name: "",
-      message: "",
-      email: "",
-    });
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-  };
-
-  const togglePopup = () => setIsPopupOpen(!isPopupOpen);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    emailjs.init({
-      publicKey: "YhN-MdfHfkjGBcWHY",
-    });
-    emailjs
-      .send(
-        "service_hragdzs", // Replace with your EmailJS service ID
-        "template_zqiimmq", // Replace with your EmailJS template ID
-        formData
-      )
-      .then(
-        (result) => {
-          alert("Message sent successfully!");
-          setFormData({ name: "", message: "", email: "" });
-          togglePopup();
-        },
-        (error) => {
-          console.log(error);
-          alert(error);
-        }
-      );
-  };
-
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
-      {/* Background Grid */}
+      {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
           src="/footer-grid.svg"
           alt="grid"
-          className="w-full h-full opacity-50"
+          className="w-full h-full opacity-50 "
         />
       </div>
 
@@ -74,15 +24,14 @@ const Footer = () => {
           Reach out to us today and let&apos;s discuss how we can help you
           achieve your goals.
         </p>
-        <button onClick={openModal}>
+        <a href="mailto:theelitesoftwaresol@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
             position="right"
           />
-        </button>
+        </a>
       </div>
-
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2024 Elite
@@ -106,94 +55,6 @@ const Footer = () => {
           </p>
         </div>
       </div>
-
-      {/* Contact Form Modal */}
-      {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-gradient-to-br from-blue-500 via-blue-600 to-blue-800 text-white p-8 rounded-lg w-full max-w-md relative animate-fadeIn">
-            <button
-              onClick={closeModal}
-              className="absolute top-2 right-2 text-white text-2xl font-bold"
-            >
-              ✖
-            </button>
-            <h2 className="text-2xl font-extrabold mb-6 text-center">
-              Contact Us
-            </h2>
-            <form onSubmit={handleSubmit} className="space-y-4">
-              <div className="flex flex-col">
-                <label htmlFor="name" className="text-sm font-semibold">
-                  Full Name
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder="Your Full Name"
-                  className="w-full mt-2 py-2 px-4 rounded-lg bg-blue-700 bg-opacity-60 placeholder-white focus:outline-none"
-                  value={formData.name}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-
-              <div className="flex flex-col">
-                <label htmlFor="email" className="text-sm font-semibold">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Your Email"
-                  className="w-full mt-2 py-2 px-4 rounded-lg bg-blue-700 bg-opacity-60 placeholder-white focus:outline-none"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              {/* <div className="flex flex-col">
-                <label htmlFor="phone" className="text-sm font-semibold">
-                  Contact Number
-                </label>
-                <input
-                  type="tel"
-                  name="phone"
-                  id="phone"
-                  placeholder="Your Contact Number"
-                  className="w-full mt-2 py-2 px-4 rounded-lg bg-blue-700 bg-opacity-60 placeholder-white focus:outline-none"
-                  value={formData.phone}
-                  onChange={handleChange}
-                  required
-                />
-              </div> */}
-
-              <div className="flex flex-col">
-                <label htmlFor="message" className="text-sm font-semibold">
-                  Your Message
-                </label>
-                <textarea
-                  name="message"
-                  id="message"
-                  placeholder="Your Message"
-                  className="w-full mt-2 py-2 px-4 rounded-lg bg-blue-700 bg-opacity-60 placeholder-white focus:outline-none"
-                  rows="4"
-                  value={formData.message}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-
-              <button
-                type="submit"
-                className="w-full py-2 rounded-lg bg-green-600 hover:bg-green-700 transition duration-300"
-              >
-                Send Message
-              </button>
-            </form>
-          </div>
-        </div>
-      )}
     </footer>
   );
 };
