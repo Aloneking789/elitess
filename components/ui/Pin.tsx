68296ad723591f835716ba1b624db01719ccfb51
Abhijeet <iamgenious21o9@gmail.com>
2024-12-10 23:19:16 +0530

    updated members

--- components/ui/Pin.tsx
+++ components/ui/Pin.tsx
@@ -18,14 +18,14 @@ export const PinContainer = ({
   containerClassName?: string;
 }) => {
   const [transform, setTransform] = useState(
-    "translate(-50%,-50%) rotateX(0deg)"
+    "translate(-50%,-50%) rotateX(2deg)"
   );
 
   const onMouseEnter = () => {
-    setTransform("translate(-50%,-50%) rotateX(40deg) scale(0.8)");
+    setTransform("translate(-50%,-50%) rotateX(0deg) scale(1.1)");
   };
   const onMouseLeave = () => {
-    setTransform("translate(-50%,-50%) rotateX(0deg) scale(1)");
+    setTransform("translate(-50%,-50%) rotateX(2deg) scale(1)");
   };
 
   return (
@@ -61,7 +61,7 @@ export const PinContainer = ({
 
 export const PinPerspective = ({
   title,
-  href,
+  href
 }: {
   title?: string;
   href?: string;
